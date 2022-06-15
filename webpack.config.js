cc86966801cb002798abc757729e5e6c8115ffcf
Alexander Huerta <alexhuerta305@gmail.com>
2022-06-15 12:52:47 -0400

    downloaded basic dependencies. Set up server and webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/src/index.jsx',
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client/dist'),
+  }
+};
+
