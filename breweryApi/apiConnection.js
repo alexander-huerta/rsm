4c340525567971c88f96aee5fd0165a725baeace
Alexander Huerta <alexhuerta305@gmail.com>
2022-06-24 11:24:46 -0400

    edited number of breweries requested from api, from 5 ro 7, to display more per page

--- breweryApi/apiConnection.js
+++ breweryApi/apiConnection.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-export const getBreweryListByCity = (city, pagecount = 5) => {
+export const getBreweryListByCity = (city, pagecount = 7) => {
 
   return axios.get(`https://api.openbrewerydb.org/breweries?by_city=${city}&per_page=${pagecount}`)
     .then(res => res.data)
